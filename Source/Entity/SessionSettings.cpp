7ccf8ed1586ef1ab30da289d502b238471018664
Tom <tom.eckerstrom@hotmail.com>
2023-03-28 10:20:47 +0200

    Added C++ classes

---
+++ Source/Entity/SessionSettings.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SessionSettings.h"
+
+SessionSettings::SessionSettings()
+{
+}
+
+SessionSettings::~SessionSettings()
+{
+}
